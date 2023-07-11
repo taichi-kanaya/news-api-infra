97938fdb8ce5c945382c73d8c40464ec7b788797
taichi-kanaya <tk.attakait@gmail.com>
2023-07-11 19:09:36 +0900

    サブネットを実装

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,3 +2,31 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+# パブリックサブネット（マルチAZ）
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/18"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+}
+resource "aws_subnet" "public_subnet_1c" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.64.0/18"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+}
+
+# プライベートサブネット（マルチAZ）
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.128.0/18"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
+resource "aws_subnet" "private_subnet_1c" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.192.0/18"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
