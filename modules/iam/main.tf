df92984106ee8697cbf9d608ba861327a7374031
taichi-kanaya <tk.attakait@gmail.com>
2023-07-26 18:08:16 +0900

    ECS用のIAMロール及びポリシーを実装

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 # GitHub Actions用IAMロール
 resource "aws_iam_role" "oidc_github_actions" {
-  name = "OIDCGithubActionsRole"
+  name = "CustomOIDCGithubActionsRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -28,7 +28,7 @@ resource "aws_iam_role" "oidc_github_actions" {
 
 # ECRにDockerイメージをPushするためのIAMポリシー
 resource "aws_iam_policy" "ecr_put_image" {
-  name = "ECRPutImagePolicy"
+  name = "CustomECRPutImagePolicy"
 
   policy = jsonencode(
     {
@@ -54,9 +54,57 @@ resource "aws_iam_policy" "ecr_put_image" {
     }
   )
 }
-
 resource "aws_iam_policy_attachment" "oidc_github_actions" {
   name       = "OIDCGithubActionsAttachment"
   roles      = [aws_iam_role.oidc_github_actions.name]
   policy_arn = aws_iam_policy.ecr_put_image.arn
+}
+
+# ECSタスク実行ロール用IAMロール
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "CustomECSTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ecs-tasks.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# ECSタスク実行ロール用IAMポリシー
+resource "aws_iam_policy" "ecs_task_execution" {
+  name = "CustomECSTaskExecutionPolicy"
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ssm:GetParameters",
+            "ecr:GetAuthorizationToken",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    }
+  )
+}
+resource "aws_iam_policy_attachment" "ecs_task_execution" {
+  name       = "OIDCGithubActionsAttachment"
+  roles      = [aws_iam_role.ecs_task_execution.name]
+  policy_arn = aws_iam_policy.ecs_task_execution.arn
 }
